88db2ccf847d8df70146e5782786203c2ca9e9a0
KASHISH GUPTA <kashish.gupta_cs.aiml24@gla.ac.in>
2026-02-12 09:23:06 +0530

    HC05

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,8 @@ void loop() {
     if (Serial.available()) {
         Serial1.write(Serial.read());
     }
-    if (Serial1.available()) {
-        Serial.write(Serial1.read());
-    }
-    if (Serial1.available()) {
-        Serial.write(Serial1.read());
 
+    if (Serial1.available()) {
+        Serial.write(Serial1.read());
     }
 }
